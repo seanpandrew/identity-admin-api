a46c404055ccfac04a2971e24318916f1a55bac8
Dan Massie <dan.massie.freelance@guardian.co.uk>
2015-10-13 10:34:14 +0100

    Don't log healthcheck requests

--- identity-admin-api/app/filters/LogRequestsFilter.scala
+++ identity-admin-api/app/filters/LogRequestsFilter.scala
@@ -6,7 +6,6 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import scala.concurrent.Future
 import Headers._
 
-/** Logs all requests, the time the app took to respond, and the status of the response */
 object LogRequestsFilter extends Filter {
 
   private val logger = Logger(this.getClass)
@@ -15,8 +14,10 @@ object LogRequestsFilter extends Filter {
     val start = System.currentTimeMillis
 
     def logTime(result: Result) {
-      val time = System.currentTimeMillis - start
-      logger.info(s"REQUEST ${rh.realRemoteAddr} ${rh.method} ${rh.uri} took $time ms and returned ${result.header.status}")
+      if(!rh.path.contains("/healthcheck")) {
+        val time = System.currentTimeMillis - start
+        logger.info(s"REQUEST ${rh.realRemoteAddr} ${rh.method} ${rh.uri} took $time ms and returned ${result.header.status}")
+      }
     }
 
     val result = f(rh)
