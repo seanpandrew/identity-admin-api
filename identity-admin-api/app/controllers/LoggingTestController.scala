d3ab664bd6515b1517b082854869143dc9339541
Dan Massie <dan.massie.freelance@guardian.co.uk>
2015-10-13 10:34:34 +0100

    Add routes for logging endpoints

--- identity-admin-api/app/controllers/LoggingTestController.scala
+++ identity-admin-api/app/controllers/LoggingTestController.scala
@@ -5,7 +5,6 @@ import play.api.mvc.{Action, Results}
 
 class LoggingTestController extends Results {
 
-  def notFound() = Action { ApiErrors.notFound }
   def badRequest() = Action { ApiErrors.badRequest("invalid") }
   def internalError() = Action { ApiErrors.internalError }
   def unauthorized() = Action { ApiErrors.unauthorized }
