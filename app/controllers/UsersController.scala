242f4f657fbae98cadde650c1c1223ff9ce37ec6
Mario Galic <mario.galic@guardian.co.uk>
2017-06-21 15:01:14 +0100

    Replace ApiResponse custom abstraction with EitherT monad

--- app/controllers/UsersController.scala
+++ app/controllers/UsersController.scala
@@ -8,7 +8,7 @@ import com.gu.tip.Tip
 import configuration.Config
 import models._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.json.{JsError, JsSuccess}
+import play.api.libs.json.{JsError, JsSuccess, Json}
 import play.api.mvc._
 import services._
 
@@ -16,7 +16,7 @@ import scala.concurrent.Future
 import scalaz.{EitherT, OptionT}
 import scalaz.std.scalaFuture._
 import models.ApiError._
-
+import models._
 
 class UserRequest[A](val user: User, request: Request[A]) extends WrappedRequest[A](request)
 
@@ -31,21 +31,24 @@ class UsersController @Inject() (
   private val MinimumQueryLength = 2
 
   def search(query: String, limit: Option[Int], offset: Option[Int]) = auth.async { request =>
-    ApiResponse {
-      if (offset.getOrElse(0) < 0) {
+    if (offset.getOrElse(0) < 0) {
 //        ApiResponse.Left(ApiErrors.badRequest("offset must be a positive integer"))
-        ApiResponse.Left(ApiError("", "offset must be a positive integer"))
-      }
-      else if (limit.getOrElse(0) < 0) {
-        ApiResponse.Left(ApiError("", "limit must be a positive integer"))
-      }
-      else if (query.length < MinimumQueryLength) {
-        ApiResponse.Left(ApiError("", s"query must be a minimum of $MinimumQueryLength characters"))
-      }
-      else {
-        userService.search(query, limit, offset)
-      }
+      Future.successful(BadRequest(Json.toJson(ApiError("", "offset must be a positive integer"))))
     }
+    else if (limit.getOrElse(0) < 0) {
+      Future.successful(BadRequest(Json.toJson(ApiError("", "limit must be a positive integer"))))
+    }
+    else if (query.length < MinimumQueryLength) {
+      Future.successful(BadRequest(Json.toJson(ApiError("", s"query must be a minimum of $MinimumQueryLength characters"))))
+    }
+    else {
+//      userService.search(query, limit, offset)
+      EitherT.fromEither(userService.search(query, limit, offset)).fold(
+        error => InternalServerError,
+        response => Ok(Json.toJson(response))
+      )
+    }
+
   }
 
   import play.api.libs.json._
@@ -63,7 +66,7 @@ class UsersController @Inject() (
       val newslettersSubF = exactTargetService.newslettersSubscription(userId)
 
       for {
-        user <- userService.findById(userId).asFuture
+        user <- userService.findById(userId)
         subscription <- subscriptionF
         membership <- membershipF
         hasCommented <- hasCommentedF
@@ -109,32 +112,33 @@ class UsersController @Inject() (
   }
 
   def update(id: String) = (auth andThen UserAction(id)).async(parse.json) { request =>
-    ApiResponse {
       request.body.validate[UserUpdateRequest] match {
         case JsSuccess(result, path) =>
           UserUpdateRequestValidator.isValid(result) match {
             case Right(validUserUpdateRequest) => {
               if (Config.stage == "PROD") Tip.verify("User Update")
               logger.info(s"Updating user id:$id, body: $result")
-              userService.update(request.user, validUserUpdateRequest)
+              EitherT.fromEither(userService.update(request.user, validUserUpdateRequest)).fold(
+                error => InternalServerError,
+                user => Ok(Json.toJson(user))
+              )
             }
             case Left(e) =>
-//              ApiResponse.Left(ApiErrors.badRequest(e.message))
-              ApiResponse.Left(ApiError("", ""))
+              //              ApiResponse.Left(ApiErrors.badRequest(e.message))
+              Future(BadRequest(Json.toJson(ApiError("", e.message))))
 
           }
-        case JsError(e) => 
-//          ApiResponse.Left(ApiErrors.badRequest(e.toString()))
-          ApiResponse.Left(ApiError("",""))
+        case JsError(e) =>
+          //          ApiResponse.Left(ApiErrors.badRequest(e.toString()))
+          Future(BadRequest(Json.toJson(ApiError("", e.toString()))))
       }
-    }
   }
 
   def delete(id: String) = (auth andThen UserAction(id)).async { request =>
     logger.info(s"Deleting user $id")
 
     def unsubscribeEmails() = EitherT(exactTargetService.unsubscribeFromAllLists(request.user.email))
-    def deleteAccount() = EitherT.fromEither(userService.delete(request.user).asFuture)
+    def deleteAccount() = EitherT.fromEither(userService.delete(request.user))
 
     (for {
       _ <- unsubscribeEmails()
@@ -155,7 +159,7 @@ class UsersController @Inject() (
   def unsubcribeFromAllEmailLists(email: String) = auth.async { request =>
     logger.info("Unsubscribing from all email lists (marketing and editorial)")
     val unsubscribeMarketingEmailsInIdentity = EitherT(exactTargetService.unsubscribeFromAllLists(email))
-    val unsubcribeAllEmailsInExactTarget = EitherT.fromEither(userService.unsubscribeFromMarketingEmails(email).asFuture())
+    val unsubcribeAllEmailsInExactTarget = EitherT.fromEither(userService.unsubscribeFromMarketingEmails(email))
 
     (for {
       _ <- unsubscribeMarketingEmailsInIdentity
@@ -195,7 +199,7 @@ class UsersController @Inject() (
 
   def sendEmailValidation(id: String) = (auth andThen UserAction(id)).async { request =>
     logger.info(s"Sending email validation for user with id: $id")
-    userService.sendEmailValidation(request.user).asFuture.map {
+    userService.sendEmailValidation(request.user).map {
       case Right(r) => NoContent
       case Left(error) => InternalServerError(Json.toJson(error))
     }
@@ -203,7 +207,7 @@ class UsersController @Inject() (
 
   def validateEmail(id: String) = (auth andThen UserAction(id)).async { request =>
     logger.info(s"Validating email for user with id: $id")
-    userService.validateEmail(request.user).asFuture.map {
+    userService.validateEmail(request.user).map {
       case Right(r) => NoContent
       case Left(error) => InternalServerError(Json.toJson(error))
     }
